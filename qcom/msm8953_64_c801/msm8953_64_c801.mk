a676d64244ab97603ec6f52f6212c5b5c3691a2b
vladimirz <vladimir.zatulovsky@micronet-inc.com>
2019-04-03 11:10:07 +0300

    add version

--- qcom/msm8953_64_c801/msm8953_64_c801.mk
+++ qcom/msm8953_64_c801/msm8953_64_c801.mk
@@ -33,7 +33,7 @@ endif
 
 # splash img
 #PRODUCT_COPY_FILES += \
-    device/qcom/msm8953_64_c801/splash.img:splash.img
+#    device/qcom/msm8953_64_c801/splash.img:splash.img
     
 TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 
@@ -88,7 +88,12 @@ $(call inherit-product, device/qcom/common/common64.mk)
 PRODUCT_NAME := msm8953_64_c801
 PRODUCT_DEVICE := msm8953_64_c801
 PRODUCT_BRAND := Android
-PRODUCT_MODEL := msm8953 for arm64
+#PRODUCT_MODEL := msm8953 for arm64
+PRODUCT_MODEL  := MSTab8
+PRODUCT_VER    := 0.0.0.1
+BUILD_DT       := $(shell date +%s)
+PRODUCT_DT     := date -d @$(BUILD_DT)
+BUILD_NUMBER   := $(shell echo $${USER:0:8}).$(PRODUCT_MODEL)_$(PRODUCT_VER)_$(shell $(PRODUCT_DT) +%Y%m%d.%H%M)
 
 PRODUCT_BOOT_JARS += tcmiface
 
