dc2f3dad9729f13419f83a3b743f4819d4698a17
vladimirz <vladimir.zatulovsky@micronet-inc.com>
2019-10-02 15:02:27 +0300

    separate userdata size for tab8 and smartcam

--- qcom/msm8953_64_c801/msm8953_64_c801.mk
+++ qcom/msm8953_64_c801/msm8953_64_c801.mk
@@ -99,7 +99,7 @@ PRODUCT_BRAND := Android
 PRODUCT_VARIANT   := $(shell echo $${PRODUCT_VARIANT})
 ifeq ($(PRODUCT_VARIANT),smartcam)
 PRODUCT_MODEL  := MSCAM
-PRODUCT_VER    := 10.2.4.1
+PRODUCT_VER    := 10.2.4.2
 PRODUCT_VARIANT := smartcam
 ifeq ($(TARGET_BUILD_VARIANT),user)
     KERNEL_DEFCONFIG := msm8953_64_c801_sc-perf_defconfig
@@ -113,7 +113,7 @@ ifeq ($(TARGET_BUILD_VARIANT),user)
 PRODUCT_VER    := 01.2.4.0
 PRODUCT_GMS_COMMON := true
 else
-PRODUCT_VER    := 00.2.4.0
+PRODUCT_VER    := 00.2.4.2
 PRODUCT_GMS_COMMON ?= false
 endif
 endif
