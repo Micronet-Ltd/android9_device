54ef1ea4ad03c00735958b5a2309ee57f7d3c7b9
vladimirz <vladimir.zatulovsky@micronet-inc.com>
2019-11-06 09:53:30 +0200

    temporary user debugging policy

--- qcom/common/udbg/udbg.sh
+++ qcom/common/udbg/udbg.sh
@@ -1,5 +1,7 @@
 #! /vendor/bin/sh
 #rm /data/vendor/kmsg.log
 #cat /dev/kmsg > /data/vendor/kmsg.log
-cat /dev/kmsg > /cache/kmsg.log
-
+#toybox cat /dev/kmsg
+#toybox_vendor cat /dev/kmsg
+toybox dmesg
+toybox_vendor dmesg
